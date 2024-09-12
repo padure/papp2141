98f79b505a6bb24f3901696a4237e779e6d24820
padure <paduregheorghe7@gmail.com>
2024-09-12 09:32:02 +0300

    Lectia 4

---
+++ lectia4/index.php
@@ -0,0 +1,12 @@
+<?php
+    const DIR = "public";
+    #Gestionarea Fisierelor
+    // Afisare directoriu curent
+    echo getcwd() . "<br>";
+    // Creare
+    // mkdir(DIR);
+    // mkdir('test');
+    // rename('test', 'test1');
+    // rmdir('test');
+    
+?>
