5070e257255388bfe52c44a9693cb3013069bfac
padure <paduregheorghe7@gmail.com>
2024-10-01 14:20:07 +0300

    Laborator 5

--- laborator5/index.php
+++ laborator5/index.php
@@ -43,7 +43,58 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        
+                        <form action="save.php" method="post">
+                            <div class="mb-3">
+                                <label for="nume">Nume</label>
+                                <input type="text" name="nume" id="nume" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="email">Email</label>
+                                <input type="email" name="email" id="email" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="parola">Parola</label>
+                                <input type="password" name="parola" id="parola" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="domiciliu">Domiciliul</label>
+                                <select name="domiciliu" id="domiciliu" class="form-control">
+                                    <option value="Chisinau">Chisinau</option>
+                                    <option value="Cahul">Cahul</option>
+                                    <option value="Balti">Balti</option>
+                                    <option value="Soroca">Soroca</option>
+                                    <option value="Comrat">Comrat</option>
+                                    <option value="Leova">Leova</option>
+                                    <option value="Ialoveni">Ialoveni</option>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <label for="genul">Genul</label>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="genul" id="masculin"
+                                        value="Masculin" checked>
+                                    <label class="form-check-label" for="masculin">
+                                        Masculin
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="genul" id="femenin"
+                                        value="Femenin">
+                                    <label class="form-check-label" for="femenin">
+                                        Femenin
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="mb-3 border-top">
+                                <div class="form-check">
+                                    <label class="form-check-label" for="da">
+                                        Aboneazate
+                                    </label>
+                                    <input class="form-check-input" type="checkbox" id="da" name="newsletter">
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-dark btn-sm">Transmite</button>
+                        </form>
                     </div>
                 </div>
             </div>
