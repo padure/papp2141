ccd97e747a79a3d9bf09b338773a552b9ab156e4
padure <paduregheorghe7@gmail.com>
2024-12-03 14:04:51 +0200

    Laravel

---
+++ laborator11/app/Http/Controllers/BookController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use App\Http\Requests\StoreBookRequest;
+use App\Http\Requests\UpdateBookRequest;
+use Illuminate\View\View;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index():View
+    {
+        $books = Book::all();
+        return view('books.index', [
+            'books' => $books
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create():View
+    {
+        return view('books.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBookRequest $request)
+    {
+        Book::create($request->all());
+        return redirect()->back();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Book $book)
+    {
+        return view('books.show', ['book' => $book]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBookRequest $request, Book $book)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Book $book)
+    {
+        //
+    }
+}
